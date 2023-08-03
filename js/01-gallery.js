0c3e0c898f9f9b5927553b9183b89e4e4fdb9b56
OlegPanadiy <opanadiy@gmail.com>
2023-08-03 22:04:31 +0100

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+function createMurkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+container.insertAdjacentHTML("beforeend", createMurkup(galleryItems));
+
+container.addEventListener("click", handlerClick);
+
+function handlerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const currentImage = evt.target.closest(".gallery__link");
+  const source = currentImage.href;
+
+  const instance = basicLightbox.create(`
+        <img src="${source}" width="800" height="600">
+    `);
+
+  instance.show();
+}
